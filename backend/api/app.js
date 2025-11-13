8b2696b3c3be9ebffd0247701692a90bd4b12937
ParasRana123 <parasrana579@gmail.com>
2025-11-13 18:36:43 +0530

    can view profile of friend also

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { clerkClient, requireAuth } from "@clerk/express";
+import { requireAuth } from "@clerk/express";
 import dotenv from "dotenv";
 import pool from "../db/dbconnection.js";
+import { clerkClient } from "@clerk/clerk-sdk-node";
 
 dotenv.config();
 
@@ -93,6 +94,7 @@ import friends from "../routes/friends.js";
 app.use("/friends", friends);
 
 import notifications from "../routes/notifications.js";
+import { use } from "react";
 app.use("/notifications", notifications);
 
 // Optional test route
@@ -104,10 +106,19 @@ app.get("/user/:id" , async (req , res) => {
   try {
     const { id } = req.params;
     const user = await pool.query("SELECT * FROM users WHERE clerk_user_id = $1" , [id]);
-    if(user.rows.length === 0) return res.status(400).json({ message: "User not found" });
-    res.json({ user: user.rows[0] });
+    if(user.rows.length === 0) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const clerkUser = await clerkClient.users.getUser(id);
+    const mergedUser = {
+      ...user.rows[0],
+      imageUrl: clerkUser.imageUrl,
+      username: clerkUser.username || clerkUser.firstName || "Music Lover",
+      email: clerkUser.primaryEmailAddress?.emailAddress
+    };
+    res.json({ user: mergedUser });
   } catch(e) {
-    console.log(err);
+    console.error("Error fetching user profile:", err);
     res.status(500).json({ message: "Server error" });
   }
 });
